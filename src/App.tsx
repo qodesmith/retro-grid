ef35806a358941b1ee11303785e90dbe3b522af6
Qodesmith <theqodesmith@gmail.com>
2026-06-06 19:32:08 -0400

    Using getRandomNumber from @qodestack/utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {useMemo} from 'react'
+import {getRandomNumber} from '@qodestack/utils'
 import Typer from './Typer'
-import {pct, px, randomNum} from './utils'
+import {pct, px} from './utils'
 import './App.scss'
 
 const NUM_OF_STARS = 1500
@@ -9,17 +10,22 @@ const NUM_OF_STARS = 1500
 export default function App() {
   const starsArray = useMemo(() => {
     return Array.from({length: NUM_OF_STARS}).map((_, i) => {
-      const top = pct(randomNum(0, 100_000) / 1000)
-      const left = pct(randomNum(0, 100_000) / 1000)
+      /**
+       * We want a random number between 1 and 100 but we want precision up to
+       * 3 decimal places, not simply whole numbers. Hence the division by 1000.
+       */
+      const top = pct(getRandomNumber(0, 100_000) / 1000)
+      const left = pct(getRandomNumber(0, 100_000) / 1000)
+      
       const opacity =
-        (randomNum(0, 15) ? randomNum(15, 30) : randomNum(50, 100)) / 100
+        (getRandomNumber(0, 15) ? getRandomNumber(15, 30) : getRandomNumber(50, 100)) / 100
       const style: Record<string, string | number> = {left, top, opacity}
 
-      if (opacity > 0.85 && !randomNum(0, 2)) {
+      if (opacity > 0.85 && !getRandomNumber(0, 2)) {
         style.width = px(3)
         style.height = px(3)
         style.boxShadow = 'white 0 0 10px,white 0 0 10px'
-      } else if (randomNum(0, 1)) {
+      } else if (getRandomNumber(0, 1)) {
         style.width = px(1)
         style.height = px(1)
       } else if (opacity > 0.75) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,3 @@
-export function randomNum(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
 export function px(num: number): string {
   return `${num}px`
 }
